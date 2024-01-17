38a634bd3381925cd02dff3319fa2fa5f4bf25db
Valerii Vatnik <zacvars@gmail.com>
2024-01-17 19:32:02 +0200

    added files for the project.

---
+++ d_ary_heap/DHeap.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d_ary_heap
+{
+    internal class DHeap
+    {
+        int d; //d value of d-ary heap
+        List<int> elements; // elements of all elements in heap
+
+        public int heapSize
+        {
+            get
+            {
+                return this.elements.Count();
+            }
+        }
+
+        public string getTree
+        {
+            get
+            {
+                string result = "";
+                int level = 0;
+                int reqForNextLvl = 0;
+                for (int i = 0; i < this.heapSize; i++)
+                {
+                    
+                    result += this.elements[i].ToString();
+                    if (i  == reqForNextLvl)
+                    {
+                        result += "\n";
+                        level++;
+                        reqForNextLvl += (int)Math.Pow(d, level);
+                    }
+                    else
+                    {
+                        if (i % d == 0)
+                        {
+                            result += "   ";
+                        }
+                        else
+                        {
+                            if(i!=heapSize-1)
+                                result += "_";
+                        }
+                    }
+
+                }
+                return result;
+            }
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            foreach (int el in elements)
+                res += el + " ";
+            return res;
+        }
+
+        #region constructors
+        public DHeap()
+        {
+            d = 2;
+            this.elements = new List<int>();
+        }
+
+        public DHeap(int d)
+        {
+            this.d = d;
+            this.elements = new List<int>();
+        }
+
+        public DHeap(int d, List<int> elements)
+        {
+            this.d = d;
+            this.elements = elements;
+            buildHeap();
+        }
+
+        public DHeap(int d, string str)
+        {
+            this.d = d;
+            elements = new List<int>();
+
+            InputElementsFromString(str);
+
+            buildHeap();
+        }
+
+        public DHeap(int d, int[] arr)
+        {
+            this.d = d;
+            elements = arr.ToList();
+
+            buildHeap();
+        }
+
+
+        #region helpful methods for constructors
+        private int InputElementsFromString(string str)
+        {
+            List<string> strings = str.Split(' ').ToList();
+            foreach (string s in strings)
+            {
+                elements.Add(int.Parse(s));
+            }
+
+            return elements.Count;
+        }
+        #endregion
+
+        #endregion
+
+
+
+
+        #region Heap Logic
+        private void swap(int indexA, int indexB)
+        {
+            int temp;
+
+            temp = elements[indexA];
+            elements[indexA] = elements[indexB];
+            elements[indexB] = temp;
+        }
+
+        public int HeapifyDown(int i)//runtime is O(log(n)/log(d))
+        {
+            int leftmostChild;
+            int rightmostChild;
+            int largestChild;
+
+            bool PlaceIsFound = false;
+            while (!PlaceIsFound)
+            {
+                leftmostChild = this.d * i + 1;
+                rightmostChild = this.d * i + this.d;
+                largestChild = i;
+
+                for (int j = leftmostChild; j <= rightmostChild && j < heapSize; j++)
+                {
+                    if (j < heapSize && elements[j] > elements[largestChild])
+                    {
+                        largestChild = j;
+                    }
+                }
+
+
+                if (largestChild != i)
+                {
+                    swap(i, largestChild);
+                    i = largestChild;
+                }
+                else
+                    PlaceIsFound = true;
+            }
+
+            return i;
+        }
+
+        public int HeapifyUp(int i)//runtime is O(log(n)/log(d))
+        {
+            int parent = (i - 1) / d;
+            while (parent >= 0)
+            {
+                if (elements[parent] < elements[i])
+                {
+                    swap(i, parent);
+                    i = parent;
+                    parent = (i - 1) / d;
+                }
+                else
+                    break;
+            }
+
+            return i;
+        }
+
+
+        private void buildHeap()
+        {
+            for (int i = (heapSize - 1) / d; i >= 0; i--)
+                HeapifyDown(i);
+        }
+
+        #endregion
+
+
+
+
+        #region The Assignment
+        public int MaxExtract() //runtime is O(d*(log(n)/log(d)))
+        {
+            if (heapSize != 0)
+            {
+                int res = elements[0];
+                elements[0] = elements[heapSize - 1];
+                elements.RemoveAt(heapSize - 1);
+                HeapifyDown(0);
+                return res;
+            }
+            else
+                return -1;
+        }
+
+        public int Insert(int value) //runtime is O(log(n)/log(d))
+        {
+            elements.Add(value);
+
+            int i = heapSize - 1;
+            return HeapifyUp(i);
+        }
+
+        public int IncreaseKey(int i, int k) //runtime is O(log(n)/log(d))
+        {
+            int index = i;
+            if (k > elements[i])
+            {
+                elements[i] = k;
+                index = HeapifyUp(i);
+                
+            }
+            return index;
+        }
+
+        #endregion
+
+
+
+    }
+}
--- d_ary_heap/Program.cs
+++ d_ary_heap/Program.cs
@@ -1,2 +1,233 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace d_ary_heap
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int d = inputD();
+
+            DHeap heap = inputStack(d);
+
+            while (true)
+            {
+                Console.WriteLine(heap.ToString());
+
+                Console.WriteLine(heap.getTree);
+
+                heap = menu(heap);
+            }
+
+        }
+
+
+        public static int inputD()
+        {
+            int d=-1000;
+            bool flagIsRead = false;
+
+            while (!flagIsRead)
+            {
+                Console.WriteLine("Input d for d-ary heap: ");
+                try
+                {
+                    d = int.Parse(Console.ReadLine());
+                    flagIsRead = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Written value is not an integer number, please try again");
+                }
+            }
+            return d;
+        }
+
+        public static DHeap inputStack(int d) {
+            DHeap heap = new DHeap();
+
+            bool inputMethodCorrect = false;
+
+            int inputMethod = 0;
+            while (!inputMethodCorrect)
+            {
+                Console.WriteLine("Choose stack input method:\n1 - via keyboard\n2 - via file");
+
+                try
+                {
+                    inputMethod = int.Parse(Console.ReadLine());
+                    if (inputMethod == 1 || inputMethod == 2)
+                        inputMethodCorrect = true;
+                    else
+                        throw new Exception("wrong input type");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("input method wasnt correctly entered. Please, try again");
+                }
+            }
+
+
+
+
+
+            bool flagStackIsReaded = false;
+            while (!flagStackIsReaded)
+            {
+                string stackLine = "";
+                switch (inputMethod)
+                {
+                    case 1:
+                        Console.WriteLine("Write stack one by one with numbers divided with space:");
+                        stackLine = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Numbers in file should be divided with spaces. Please input filename:");
+                        stackLine = File.ReadAllText(Console.ReadLine());
+
+                        break;
+                    default:
+                        break;
+                }
+
+                try
+                {
+                    if (stackLine != "" || stackLine != null)
+                    {
+                        heap = new DHeap(d, stackLine);
+                        flagStackIsReaded = true;
+                    }
+                    else
+                    {
+                        throw new Exception("Stack line is empty");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("There is an error in stack input. please, try input it again.");
+                }
+            }
+
+            return heap;
+        }
+
+
+        public static DHeap menu(DHeap heap)
+        {
+
+            
+
+            bool MenuItemCorrect = false;
+
+            int MenuItemChosen = 0;
+            while (!MenuItemCorrect)
+            {
+                Console.WriteLine("1 - MAX-EXTRACT");
+                Console.WriteLine("2 - INSERT");
+                Console.WriteLine("3 - INCREASE-KEY");
+                Console.WriteLine("0 - close app");
+
+                int[] MenuOptions = {1, 2, 3, 0};
+                try
+                {
+                    MenuItemChosen = int.Parse(Console.ReadLine());
+                    if (MenuOptions.Contains(MenuItemChosen))
+                        MenuItemCorrect = true;
+                    else
+                        throw new Exception("wrong Menu Item");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("There is no such item in the menu. Please, try again");
+                }
+            }
+
+
+            switch (MenuItemChosen)
+            {
+                case 1:
+                    Console.WriteLine("Extracted Max was: " + heap.MaxExtract());
+                    break;
+
+                case 2:
+                    {
+                        int n = -1000;
+                        bool IsNumber = false;
+
+                        while(!IsNumber)
+                        {
+                            Console.WriteLine("Input number to insert");
+
+                            try
+                            {
+                                n = int.Parse(Console.ReadLine());
+                                IsNumber = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("It wasn't a number. Please, try again.");
+                            }
+                        }
+
+                        heap.Insert(n);
+                        break;
+                    }
+
+                case 3:
+                    {
+                        int i = -1000;
+                        int k = -1000;
+                        bool IsNumber = false;
+
+                        while (!IsNumber)
+                        {
+                            Console.WriteLine(heap.ToString());
+                            Console.WriteLine("Input index to increase key in");
+
+                            try
+                            {
+                                i = int.Parse(Console.ReadLine());
+                                IsNumber = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("It wasn't a number. Please, try again.");
+                            }
+                        }
+
+
+                        IsNumber = false;
+                        while (!IsNumber)
+                        {
+                            Console.WriteLine("Input Key Value to try to increase key in " + i);
+
+                            try
+                            {
+                                k = int.Parse(Console.ReadLine());
+                                IsNumber = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("It wasn't a number. Please, try again.");
+                            }
+                        }
+
+                        heap.IncreaseKey(i, k);
+                        break;
+                    }
+
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+            }
+
+            return heap;
+
+        }
+
+        
+
+    }
+}
